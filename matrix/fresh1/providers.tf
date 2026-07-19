fbed5616b3dd89c5038fbafe8f1ec9cc155fb39c
thesystemsguy06 <112845572+thesystemsguy06@users.noreply.github.com>
2026-07-18 20:37:54 -0700

    test(fresh1): violating base providers.tf

---
+++ matrix/fresh1/providers.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws    = { source = "hashicorp/aws", version = "~> 5.0" }
+    random = { source = "hashicorp/random", version = "~> 3.5" }
+  }
+  backend "s3" {
+    bucket  = "vp-test-data-746210888062"
+    key     = "matrix/fresh1/terraform.tfstate"
+    region  = "us-east-1"
+    profile = "vp-target-user"
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "vp-target-user"
+  default_tags {
+    tags = {
+      Project = "vectorplane-e2e-test"
+      Wave    = "matrix-fresh1"
+    }
+  }
+}
+
+resource "random_id" "s" {
+  byte_length = 4
+}
