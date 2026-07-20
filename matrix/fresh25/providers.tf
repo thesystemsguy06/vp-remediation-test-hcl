28458b43d84c07bbace08d2dc2d49ecb1578c592
thesystemsguy06 <112845572+thesystemsguy06@users.noreply.github.com>
2026-07-20 11:22:46 -0700

    test(fresh25): violating fixture

---
+++ matrix/fresh25/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws     = { source = "hashicorp/aws", version = "~> 5.0" }
+    random  = { source = "hashicorp/random", version = "~> 3.5" }
+    archive = { source = "hashicorp/archive", version = "~> 2.4" }
+  }
+  backend "s3" {
+    bucket  = "vp-test-data-746210888062"
+    key     = "matrix/fresh25/terraform.tfstate"
+    region  = "us-east-1"
+    profile = "vp-target-user"
+  }
+}
+provider "aws" {
+  region  = "us-east-1"
+  profile = "vp-target-user"
+  default_tags { tags = { Project = "vectorplane-e2e-test", Wave = "matrix-fresh25" } }
+}
+resource "random_id" "s" { byte_length = 4 }
