7a3c47c897608eda9466f2ef831bd637da40d82a
thesystemsguy06 <112845572+thesystemsguy06@users.noreply.github.com>
2026-07-20 08:37:05 -0700

    test(fresh19): violating fixture

---
+++ matrix/fresh19/main.tf
@@ -0,0 +1,23 @@
+data "aws_vpc" "d" {
+  default = true
+}
+data "aws_subnets" "d" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.d.id]
+  }
+}
+resource "aws_docdb_subnet_group" "vp" {
+  name       = "vp-fresh19-${random_id.s.hex}"
+  subnet_ids = slice(tolist(data.aws_subnets.d.ids), 0, 2)
+}
+resource "aws_docdb_cluster" "vp" {
+  cluster_identifier      = "vp-fresh19-${random_id.s.hex}"
+  master_username         = "vpadmin"
+  master_password         = "ChangeMe123456"
+  db_subnet_group_name    = aws_docdb_subnet_group.vp.name
+  storage_encrypted       = true
+  skip_final_snapshot     = true
+  backup_retention_period = 1
+  apply_immediately       = true
+}
