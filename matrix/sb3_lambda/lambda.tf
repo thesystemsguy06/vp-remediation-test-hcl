ed481dda7848ef9a8888ca3afa7fea66ecae8143
thesystemsguy06 <112845572+thesystemsguy06@users.noreply.github.com>
2026-07-21 17:45:51 -0700

    test(sb3_lambda): violating fixture

---
+++ matrix/sb3_lambda/lambda.tf
@@ -0,0 +1,29 @@
+data "archive_file" "fn" {
+  type        = "zip"
+  output_path = "${path.module}/fn_${random_id.s.hex}.zip"
+  source {
+    content  = "def handler(e,c): return 'ok'"
+    filename = "index.py"
+  }
+}
+
+resource "aws_iam_role" "lambda" {
+  name = "vp-sb3-lambda-${random_id.s.hex}"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_lambda_function" "this" {
+  function_name    = "vp-sb3-fn-${random_id.s.hex}"
+  role             = aws_iam_role.lambda.arn
+  handler          = "index.handler"
+  runtime          = "python3.12"
+  filename         = data.archive_file.fn.output_path
+  source_code_hash = data.archive_file.fn.output_base64sha256
+}
