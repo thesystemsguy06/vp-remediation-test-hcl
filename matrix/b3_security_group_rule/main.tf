e3fcb76079f36737ee9d8c17105642a5d454903b
thesystemsguy06 <112845572+thesystemsguy06@users.noreply.github.com>
2026-07-22 19:51:42 -0700

    test(b3_security_group_rule): violating fixture

---
+++ matrix/b3_security_group_rule/main.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "vp_b3" {
+  name        = "vp-b3-sg-rules"
+  description = "vp b3 test open rules"
+  vpc_id      = "vpc-0880cc850def460a5"
+}
+resource "aws_security_group_rule" "ssh_v4" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.vp_b3.id
+}
+resource "aws_security_group_rule" "rdp_v4" {
+  type              = "ingress"
+  from_port         = 3389
+  to_port           = 3389
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.vp_b3.id
+}
+resource "aws_security_group_rule" "ssh_v6" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  ipv6_cidr_blocks  = ["::/0"]
+  security_group_id = aws_security_group.vp_b3.id
+}
